63eeb8b254c58ffa5f0200f7b8c4de1b1ce94e4e
unknown <lakshminaba10.oracle@gmail.com>
2023-12-06 22:40:21 +0530

    first commit

---
+++ firstapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .forms import StudentForm
+
+# Create your views here.
+def display(request):
+    if request.method=='GET':
+        form=StudentForm()
+        return render(request,'index.html',{'form':form})
+    else:
+        form=StudentForm(request.POST)
+        if form.is_valid:
+            form.save(commit=True)
+        return render(request,'index.html',{'form':form})
+        
